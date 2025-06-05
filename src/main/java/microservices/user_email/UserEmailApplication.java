dcc49ff50e539dd55804b3416007c4b4c1af925d
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-06-05 15:57:45 -0300

    ms_user

---
+++ src/main/java/microservices/user_email/UserEmailApplication.java
@@ -0,0 +1,13 @@
+package microservices.user_email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/microservices/user_email/UserEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package microservices.user_email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
