fbec71925f92315eb43fdd0fd7a98fa13298fd6f
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-06-06 08:00:42 -0300

    feat: finish the configuration

--- src/main/java/microservices/user_email/configs/RabbitMQconfig.java
+++ src/main/java/microservices/user_email/configs/RabbitMQconfig.java
@@ -1,4 +1,19 @@
 package microservices.user_email.configs;
 
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class RabbitMQconfig {
+
+    @Value("${broker.queue.email.name}")
+    private String queue;
+
+    @Bean
+    public Queue queue (){
+        return  new  Queue(queue,true);
+    }
 }
