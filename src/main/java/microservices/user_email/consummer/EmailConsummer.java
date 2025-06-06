f36e19b57abc00bfa6bba13c34e142e732601dbe
Mateus web developer <mateussoftwaredeveloper@gmail.com>
2025-06-06 08:25:54 -0300

    feat: queue tested

--- src/main/java/microservices/user_email/consummer/EmailConsummer.java
+++ src/main/java/microservices/user_email/consummer/EmailConsummer.java
@@ -1,4 +1,15 @@
 package microservices.user_email.consummer;
 
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
 public class EmailConsummer {
+
+    @RabbitListener(queues =  "${broker.queue.email.name}")
+    public void  ListennerEmailQueue(@Payload String string){
+        System.out.println(string);
+
+    }
 }
